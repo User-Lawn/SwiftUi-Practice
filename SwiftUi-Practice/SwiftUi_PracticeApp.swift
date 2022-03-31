61c34a6de6313bf5430001128cae83144eb9f131
LAWN <user.lawn@gmail.com>
2022-03-31 13:06:31 +0900

    Initial Commit

---
+++ SwiftUi-Practice/SwiftUi_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_PracticeApp.swift
+//  SwiftUi-Practice
+//
+//  Created by Jun.Mac on 2022/03/31.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
