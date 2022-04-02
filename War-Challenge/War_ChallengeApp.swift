b059a0685b93152d842fe94543b578cf1c3707d5
LAWN <user.lawn@gmail.com>
2022-04-02 17:15:29 +0900

    war challenge

---
+++ War-Challenge/War_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_ChallengeApp.swift
+//  War-Challenge
+//
+//  Created by Jun.Mac on 2022/04/02.
+//
+
+import SwiftUI
+
+@main
+struct War_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
