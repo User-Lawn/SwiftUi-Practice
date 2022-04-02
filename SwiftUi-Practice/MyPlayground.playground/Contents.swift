7406a1ca8815d38baa844813e99bcf7ebacdac82
LAWN <user.lawn@gmail.com>
2022-04-02 18:32:17 +0900

    learn basic

---
+++ SwiftUi-Practice/MyPlayground.playground/Contents.swift
@@ -0,0 +1,25 @@
+//// String. Xcode won't treat this as code
+//"hello"
+//
+//// Int
+//12
+//10
+//
+//// Double
+//0.4
+//1.2
+//
+//// Bool
+//true
+//false
+
+//var myVar: String = "hello"
+//myVar = "world"
+//    print(myVar)
+
+//var myInt:Int = 100
+//    print(myInt)
+
+
+//let myVar: String = "hello"
+//print(myVar)
