e0c63cfba1553553ce44e61f97c858165ecf0f74
LAWN <user.lawn@gmail.com>
2022-04-06 13:15:07 +0900

    login

---
+++ come-back-home-lawn/come-back-home-lawn/come_back_home_lawnApp.swift
@@ -0,0 +1,17 @@
+//
+//  come_back_home_lawnApp.swift
+//  come-back-home-lawn
+//
+//  Created by Jun.Mac on 2022/04/06.
+//
+
+import SwiftUI
+
+@main
+struct come_back_home_lawnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
