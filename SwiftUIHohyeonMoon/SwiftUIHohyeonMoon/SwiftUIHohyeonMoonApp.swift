59f1e5b678f90de4ab3fcc1ae884b0c2d2de87a2
이준영 <74142881+User-Lawn@users.noreply.github.com>
2022-04-11 22:09:51 +0900

    learn text

---
+++ SwiftUIHohyeonMoon/SwiftUIHohyeonMoon/SwiftUIHohyeonMoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIHohyeonMoonApp.swift
+//  SwiftUIHohyeonMoon
+//
+//  Created by Jun.Mac on 2022/04/11.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIHohyeonMoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIHohyeonMoon/SwiftUIHohyeonMoon/Text.swift
@@ -0,0 +1,23 @@
+//
+//  Text.swift
+//  SwiftUIHohyeonMoon
+//
+//  Created by Jun.Mac on 2022/04/11.
+//
+
+import SwiftUI
+
+struct Text: View {
+    var body: some View {
+        
+        Text("H")
+            .padding()
+    }
+}
+    
+struct Text_Previews: PreviewProvider {
+        static var previews: some View {
+            Text()
+        }
+    }
+
